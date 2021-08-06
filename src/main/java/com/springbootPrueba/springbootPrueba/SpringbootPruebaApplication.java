d7b6ddb26fe9a78ade58debed2be6e2e6ed04995
Ricardo Candel <rcandelg@gmail.com>
2021-08-06 15:42:13 +0200

    Inicio del proyecto de la prueba

---
+++ src/main/java/com/springbootPrueba/springbootPrueba/SpringbootPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.springbootPrueba.springbootPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootPrueba/springbootPrueba/SpringbootPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootPrueba.springbootPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
